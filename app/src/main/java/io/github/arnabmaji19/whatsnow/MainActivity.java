7a38b6f6c2d7df6c297e16482ef167849f219fc5
Arnab Maji <arnabmaji1999@gmail.com>
2019-12-08 14:47:30 +0530

    add navigation drawer with custom toolbar

--- app/src/main/java/io/github/arnabmaji19/whatsnow/MainActivity.java
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/MainActivity.java
@@ -2,7 +2,10 @@ package io.github.arnabmaji19.whatsnow;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createNavigationDrawer();
+    }
 
+    private void createNavigationDrawer() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
