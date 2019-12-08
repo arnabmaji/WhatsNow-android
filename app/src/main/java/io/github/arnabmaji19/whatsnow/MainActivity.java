0068cfe6bb355d40260e022184326f91fb7c4b70
Arnab Maji <arnabmaji1999@gmail.com>
2019-12-08 15:37:28 +0530

    add ui: dashboard

--- app/src/main/java/io/github/arnabmaji19/whatsnow/MainActivity.java
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/MainActivity.java
@@ -1,24 +1,59 @@
 package io.github.arnabmaji19.whatsnow;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createNavigationDrawer();
+        setUpNavigationDrawer();
+
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.dashboard:
+                        selectedFragment = new DashboardFragment();
+                        break;
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frame_layout, selectedFragment)
+                            .commit();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 
-    private void createNavigationDrawer() {
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+    private void setUpNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
