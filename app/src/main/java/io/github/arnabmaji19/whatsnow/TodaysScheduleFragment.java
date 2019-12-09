7a06bbb570292075d952bb9045f0d07f7bef1934
Arnab Maji <arnabmaji1999@gmail.com>
2019-12-09 10:23:18 +0530

    create today's schedule ui

--- app/src/main/java/io/github/arnabmaji19/whatsnow/TodaysScheduleFragment.java
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/TodaysScheduleFragment.java
@@ -9,13 +9,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import ca.antonious.materialdaypicker.MaterialDayPicker;
+import ca.antonious.materialdaypicker.SingleSelectionMode;
+
 public class TodaysScheduleFragment extends Fragment {
+
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragement_todays_schedule, container, false);
+        view = inflater.inflate(R.layout.fragement_todays_schedule, container, false);
+
+        //Configuring Week Day picker
+        MaterialDayPicker dayPicker = view.findViewById(R.id.day_picker);
+        dayPicker.setSelectionMode(SingleSelectionMode.create());
+
         return view;
     }
 }
