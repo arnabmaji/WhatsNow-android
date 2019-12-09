c8e7ba320095a77328d3fe0d899cfa07a1daf5fd
Arnab Maji <arnabmaji1999@gmail.com>
2019-12-09 15:23:08 +0530

    add pojo class: Lecture and Schedule

---
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/model/Lecture.java
@@ -0,0 +1,72 @@
+package io.github.arnabmaji19.whatsnow.model;
+
+//Lecture POJO class for retrieving data from database and app usage
+
+public class Lecture {
+    private int period;
+    private String courseName;
+    private String courseCode;
+    private String faculty;
+    private String room;
+    private String duration;
+
+    public Lecture() {
+    }
+
+    public Lecture(int period, String courseName, String courseCode, String faculty, String room, String duration) {
+        this.period = period;
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.faculty = faculty;
+        this.room = room;
+        this.duration = duration;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
---
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/model/Schedule.java
@@ -0,0 +1,73 @@
+package io.github.arnabmaji19.whatsnow.model;
+
+//Schedule PJO class for retrieving data from database
+
+public class Schedule {
+
+    private String department;
+    private String batch;
+    private String section;
+    private String semester;
+    private String scheduleId;
+    private String lastUpdated;
+
+    public Schedule() {
+    }
+
+    public Schedule(String department, String batch, String section, String semester, String scheduleId, String lastUpdated) {
+        this.department = department;
+        this.batch = batch;
+        this.section = section;
+        this.semester = semester;
+        this.scheduleId = scheduleId;
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public String getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(String lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
