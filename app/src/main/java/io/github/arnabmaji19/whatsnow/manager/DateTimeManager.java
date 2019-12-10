ddca780c777336aaf340e5fa7f8547815bb7427d
Arnab Maji <arnabmaji1999@gmail.com>
2019-12-10 14:59:19 +0530

    create DateTimeManager class

---
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/manager/DateTimeManager.java
@@ -0,0 +1,87 @@
+package io.github.arnabmaji19.whatsnow.manager;
+
+//Manages Data Time based operations
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DateTimeManager {
+
+    private static final int PERIOD_MAX_TIME = 55;
+
+    public static final String[] WEEKDAYS = new String[]{
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday"
+    };
+
+    public enum Weekdays {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY}
+
+    private String timeString;
+    private int currentTimeValue;
+    private String periodStartTimeString;
+
+    public DateTimeManager() {
+        Calendar calendar = Calendar.getInstance();
+        //Calculating current time values
+        int currentMinute = calendar.get(Calendar.MINUTE);
+        int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        this.timeString = String.format(Locale.getDefault(), "%d%02d", currentHour, currentMinute);
+        this.currentTimeValue = Integer.parseInt(timeString);
+    }
+
+    public int getCurrentLectureNo() {
+        int currentPeriod = -1;
+        int startTime = 0;
+        if (currentTimeValue >= 900 && currentTimeValue < 955) {
+            currentPeriod = 1;
+            startTime = 900;
+        } else if (currentTimeValue >= 955 && currentTimeValue < 1050) {
+            currentPeriod = 2;
+            startTime = 955;
+        } else if (currentTimeValue >= 1050 && currentTimeValue < 1145) {
+            currentPeriod = 3;
+            startTime = 1050;
+        } else if (currentTimeValue >= 1145 && currentTimeValue < 1240) {
+            currentPeriod = 4;
+            startTime = 1145;
+        } else if (currentTimeValue >= 1240 && currentTimeValue < 1335) {
+            currentPeriod = 5;
+            startTime = 1240;
+        } else if (currentTimeValue >= 1335 && currentTimeValue < 1430) {
+            currentPeriod = 6;
+            startTime = 1335;
+        } else if (currentTimeValue >= 1430 && currentTimeValue < 1525) {
+            currentPeriod = 7;
+            startTime = 1430;
+        } else if (currentTimeValue >= 1525 && currentTimeValue < 1620) {
+            currentPeriod = 8;
+            startTime = 1525;
+        }
+        periodStartTimeString = String.format(Locale.getDefault(), "%04d", startTime);
+        return currentPeriod;
+    }
+
+    public double getElapsedTimeFraction() {
+        String currentTimeString = String.format(Locale.getDefault(), "%04d", this.currentTimeValue);
+        String startTime = periodStartTimeString;
+        int currentHour = Integer.parseInt(currentTimeString.substring(0, 2));
+        int currentMinute = Integer.parseInt(currentTimeString.substring(2));
+
+        int startHour = Integer.parseInt(startTime.substring(0, 2));
+        int startMinute = Integer.parseInt(startTime.substring(2));
+        int elapsedMinute = 0;
+        if (currentMinute >= startMinute) {
+            elapsedMinute += (currentMinute - startMinute);
+        } else {
+            currentHour--;
+            currentMinute += 60;
+            elapsedMinute += (currentMinute - startMinute);
+        }
+        int elapsedTime = elapsedMinute + (currentHour - startHour) * 60;
+
+        return elapsedTime / PERIOD_MAX_TIME;
+    }
+}
---
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/manager/ScheduleManager.java
@@ -0,0 +1,6 @@
+package io.github.arnabmaji19.whatsnow.manager;
+
+//Manages different schedule operations
+
+public class ScheduleManager {
+}
