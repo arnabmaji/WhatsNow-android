13851d07ffc0e4dff72e44242b7359ec0b543392
Arnab Maji <arnabmaji1999@gmail.com>
2019-12-15 22:47:50 +0530

    create settings activity layout with preference lib

--- app/src/main/java/io/github/arnabmaji19/whatsnow/SettingsActivity.java
+++ app/src/main/java/io/github/arnabmaji19/whatsnow/SettingsActivity.java
@@ -2,7 +2,10 @@ package io.github.arnabmaji19.whatsnow;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,7 +13,28 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        //display settings fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
 
+        //configure toolbar
+        Toolbar toolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
+    }
+
+    //Settings Fragment
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.settings_preferences, rootKey);
+        }
     }
 }
